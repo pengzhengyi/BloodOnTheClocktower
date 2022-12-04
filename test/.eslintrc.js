283681ab6f97b7a410137dce05e24fe2ab1c8386
Zhengyi Peng <pengzhengyipengzhengyi@gmail.com>
2022-12-04 04:56:44 +0000

    chore: override some eslint rules in test folder and mocks folder

--- test/.eslintrc.js
+++ test/.eslintrc.js
@@ -1,9 +1,11 @@
 module.exports = {
   overrides: [
     {
-      files: ['*.test.ts'],
+      files: ['*.test.ts', '__mocks__/*.ts'],
       rules: {
         'import/first': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        'import/order': 'warn',
       },
     },
   ],
