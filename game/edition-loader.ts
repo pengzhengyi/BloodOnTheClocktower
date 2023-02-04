5559514340c0fd53fac0a4c0fcecc581abc0063e
Zhengyi Peng <pengzhengyipengzhengyi@gmail.com>
2023-02-04 03:34:50 +0000

    refactor: extract inteface for edition loader

--- game/edition-loader.ts
+++ game/edition-loader.ts
@@ -1,8 +1,11 @@
-import axios, { AxiosRequestConfig } from 'axios';
 import { Edition } from './edition';
-import { EditionData } from './types';
-import { EditionLoadFailure, NoEditionMatchingName } from './exception';
+import { NoEditionMatchingName } from './exception';
 import { NAME_TO_EDITION } from '~/content/editions/editions';
+
+export interface IEditionLoader {
+    tryLoad(editionName: string): typeof Edition | undefined;
+    loadAsync(editionName: string): Promise<typeof Edition>;
+}
 
 export abstract class EditionLoader {
     static tryLoad(editionName: string): typeof Edition | undefined {
@@ -16,37 +19,5 @@ export abstract class EditionLoader {
         );
 
         return this.tryLoad(error.correctedEditionName)!;
-    }
-
-    protected static loadWithEditionData(editionData: Partial<EditionData>) {
-        const edition = this.loadEditionClass();
-        edition.initialize(editionData);
-        return edition;
-    }
-
-    protected static loadEditionClass(): typeof Edition {
-        return class extends Edition {};
-    }
-
-    protected static async loadEditionEditionDataAsync(
-        editionName: string
-    ): Promise<Partial<EditionData>> {
-        const config: AxiosRequestConfig = {};
-
-        return await axios
-            .get<Partial<EditionData>>(
-                this.getEditionEditionDataApiEndpoint(editionName),
-                config
-            )
-            .catch((error) => {
-                throw new EditionLoadFailure(editionName, error);
-            })
-            .then((response) => response.data);
-    }
-
-    protected static getEditionEditionDataApiEndpoint(
-        editionName: string
-    ): string {
-        return `/api/_content/editions/${editionName}`;
     }
 }
