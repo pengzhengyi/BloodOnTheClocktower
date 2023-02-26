9dfab7c98c333a60029d4ec48d97982851252143
Zhengyi Peng <pengzhengyipengzhengyi@gmail.com>
2023-02-26 09:33:45 +0000

    refactor: correct import

--- game/storyline/act.ts
+++ game/storyline/act.ts
@@ -1,4 +1,4 @@
-import type { IGame } from './game';
+import type { IGame } from '../game';
 
 /**
  * An act is the smallest component of a storyline (or less figuratively, an act is an event in the Blood on the Clocktower game). It can be anything from a character’s ability at night to an execution on the day.
