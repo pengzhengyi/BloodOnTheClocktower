c4a652e04eb5caf45895b90ac95b50ec9a73ee56
Zhengyi Peng <pengzhengyipengzhengyi@gmail.com>
2023-03-29 04:14:10 +0000

    test: improve testing for dead player cannot vote for execution

--- test/player.test.ts
+++ test/player.test.ts
@@ -1,14 +1,11 @@
 import { faker } from '@faker-js/faker';
 import { playerFromDescription } from './utils';
-import {
-    mockPlayer,
-    mockDeadPlayer,
-    createBasicPlayer,
-} from '~/__mocks__/player';
+import { mockPlayer, createBasicPlayer } from '~/__mocks__/player';
 import { mockExecution } from '~/__mocks__/execution';
 import { Imp, Scapegoat, Washerwoman } from '~/__mocks__/character';
 import { storytellerHandleMock } from '~/__mocks__/game-ui';
 import { PlayerCannotExile } from '~/game/exception/player-cannot-exile';
+import { DeadReason } from '~/game/dead-reason';
 
 describe('test Player serialization', () => {
     test.concurrent('convert to object', async () => {
@@ -27,7 +24,8 @@ describe('test Player serialization', () => {
 
 describe('test Player Edge Cases', () => {
     test('Only alive players may nominate.', async () => {
-        const nominator = mockDeadPlayer();
+        const nominator = await createBasicPlayer(undefined, Washerwoman);
+        await nominator.setDead(DeadReason.Other);
         const nominated = mockPlayer();
         const execution = mockExecution();
 
